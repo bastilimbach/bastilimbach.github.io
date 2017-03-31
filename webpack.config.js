7461c63a9d2f6d57995e67ba3ba47a52c1891dbc
Sebastian Limbach <bastilimbach@gmail.com>
2017-03-31 21:11:34 +0200

    Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,25 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['env', {
+                                targets: {
+                                    browsers: '> 5%'
+                                }
+                            }]
+                        ]
+                    }
+                }
+            }
+        ]
     }
 }
